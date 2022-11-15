3afc31dd735892c4de9592f9a4c0b30e275b8a78
Тимофей <timaknizh@gmail.com>
2022-11-15 11:09:14 +0300

    сделал задание

--- src/Ai95Calculator.java
+++ src/Ai95Calculator.java
@@ -2,7 +2,7 @@ class Ai95Calculator {
     double price = 46.35;
 
     public double calculate(double volume, double discount) {
-        double fullPrice = price * (1 + discount);
+        double fullPrice = price * (1 - discount);
         return volume * fullPrice;
     }
 }
